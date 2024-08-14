61158d4b04dde2cb92ad8ff9c87316e9c863ac09
BiswajitxDas <110236626+BiswajitxDas@users.noreply.github.com>
2024-08-14 19:11:38 +0530

    pagination and style changes

--- Script_panel_backend/Script_panel/views.py
+++ Script_panel_backend/Script_panel/views.py
@@ -1,9 +1,6 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 from django.core.paginator import Paginator
 import sys
-import pandas as pd
-import time
 
 sys.path.append('/path/to/VOTER_DATA_BACKUP')
 from VOTER_DATA_BACKUP.FUNCTION.test import end_df
@@ -19,24 +16,24 @@ def run_script(request):
         # Sort the DataFrame by specific columns
         sorted_df = df.sort_values(by=['AC_ID', 'PC_ID'])
 
-        # Convert DataFrame to list of dictionaries
-        table_data = sorted_df.to_dict(orient='records')
-        
-        # Dynamically get table headers
+        # Convert DataFrame to a list of lists for easier rendering in the template
+        table_data = sorted_df.values.tolist()
+
+        # Get table headers
         headers = sorted_df.columns.tolist()
 
-        # Implement pagination
-        paginator = Paginator(table_data, 10)  # Show 10 items per page
+        # Set up pagination
+        paginator = Paginator(table_data, 50)  # 50 items per page
         page_number = request.GET.get('page')
         page_obj = paginator.get_page(page_number)
-
+        totalpages = page_obj.paginator.num_pages
         context = {
-            'table_data' : table_data,
-            'page_obj': page_obj,
             'headers': headers,
-            'success_message': 'Data updated successfully!'
+            'page_obj': page_obj,
+            'totalpages': [n+1 for n in range(totalpages)],
+            'success_message': 'Data updated successfully!',
         }
-        
+
         return render(request, 'home_page.html', context)
 
     return render(request, 'home_page.html')
